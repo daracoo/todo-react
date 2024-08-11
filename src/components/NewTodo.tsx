84bdd082b7e3be77434db868fb8ece1599614862
daracoo <dkuzmanoski21@gmail.com>
2024-08-11 23:36:30 +0200

    Added ref

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,11 +1,24 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+
     const submitHandler = (event: React.FormEvent) => { // since it's for form we use FormEvent, if we have onClick -> MouseEvent
         event.preventDefault();
+
+        const enteredText = todoTextInputRef.current?.value
+
+        if(enteredText?.trim().length === 0){
+            return;
+        }
+
+        
     }
 
     return <form onSubmit={submitHandler}>
         <label htmlFor="text">Todo text</label>
-        <input type="text" id="text"/>
+        <input type="text" id="text" ref={todoTextInputRef}/>
         <button>Add Todo</button>
     </form>
 }
